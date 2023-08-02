e572981a049ea224726f04da9f6c2102e972d9a8
mosta <mostafamohamedit1@gmail.com>
2023-08-02 23:02:03 +0300

    foodapp

--- app/src/main/java/com/mostafa/foodapp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/mostafa/foodapp/ui/activity/MainActivity.kt
@@ -3,61 +3,64 @@ package com.mostafa.foodapp.ui.activity
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.mostafa.foodapp.R
 import com.mostafa.foodapp.data.model.Categories
 import com.mostafa.foodapp.data.netWorking.ApiHelper
 import com.mostafa.foodapp.data.netWorking.RetrofitBuilder
-import com.mostafa.foodapp.ui.viewModel.CategoryViewModel
 import com.mostafa.foodapp.databinding.ActivityMainBinding
 import com.mostafa.foodapp.ui.adapter.CategoryRecyclerView
+import com.mostafa.foodapp.ui.viewModel.CategoryViewModel
+
 import com.mostafa.foodapp.ui.viewModel.CategoryViewModelFactory
-import kotlinx.coroutines.DelicateCoroutinesApi
-
-abstract class MainActivity() : AppCompatActivity() {
-    lateinit var binding:ActivityMainBinding
-    lateinit var viewModel: CategoryViewModel
-
-    var categoriesli: ArrayList<Categories.Category> = ArrayList()
-     val categoryRecyclerView: CategoryRecyclerView by lazy { CategoryRecyclerView(allData = ArrayList()) }
-    @SuppressLint("SuspiciousIndentation")
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
-        setSupportActionBar(binding.toolbar)
-
-//        val linearManger=LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
-//        binding.recycler.layoutManager=linearManger
-//        binding.recycler.setHasFixedSize(true)
-//        categoryRecyclerView.setCategoryList(categorical)
-//        val linearLayoutManager = LinearLayoutManager(this)
-//        linearLayoutManager.orientation = LinearLayoutManager.VERTICAL
-//        binding.recycler.adapter=categoryRecyclerView
-//        binding.recycler.setLayoutManager(linearLayoutManager)
-//        binding.recycler.addItemDecoration(
-//            DividerItemDecoration(
-//                this@MainActivity,
-//                LinearLayoutManager.VERTICAL
-//            )
-//        )
 
 
-        viewModel = ViewModelProvider(this,CategoryViewModelFactory(
-            ApiHelper(RetrofitBuilder.apiService)))[CategoryViewModel::class.java]
+class MainActivity() : AppCompatActivity() {
+        lateinit var binding: ActivityMainBinding
+        lateinit var viewModel: CategoryViewModel
+
+        var categoriesli: ArrayList<Categories.Category> = ArrayList()
+        val categoryRecyclerView: CategoryRecyclerView by lazy { CategoryRecyclerView(allData = ArrayList()) }
+
+        @SuppressLint("SuspiciousIndentation")
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+            setSupportActionBar(binding.toolbar)
+
+        val linearManger=LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
+        binding.recycler.layoutManager=linearManger
+        binding.recycler.setHasFixedSize(true)
+        val linearLayoutManager = LinearLayoutManager(this)
+        linearLayoutManager.orientation = LinearLayoutManager.VERTICAL
         binding.recycler.adapter=categoryRecyclerView
+        binding.recycler.setLayoutManager(linearLayoutManager)
+        binding.recycler.addItemDecoration(
+            DividerItemDecoration(
+                this@MainActivity,
+                LinearLayoutManager.VERTICAL
+            )
+        )
 
-        viewModel.category.observe(this@MainActivity, Observer { categories ->
-            categoryRecyclerView.setCategoryList(categoriesli)
-         }
+
+            viewModel = ViewModelProvider(
+                this, CategoryViewModelFactory(
+                    ApiHelper(RetrofitBuilder.apiService)
+                )
+            )[CategoryViewModel::class.java]
+                binding.recycler.adapter=categoryRecyclerView
+
+            viewModel.category.observe(this@MainActivity, Observer { categories ->
+                categoryRecyclerView.setCategoryList(categoriesli)
+            }
             )
 
-}
+        }
 
 
     }
-
--- app/src/main/java/com/mostafa/foodapp/ui/adapter/CategoryRecyclerView.kt
+++ app/src/main/java/com/mostafa/foodapp/ui/adapter/CategoryRecyclerView.kt
@@ -1,66 +1,45 @@
 package com.mostafa.foodapp.ui.adapter
 
-import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.ImageView
-import androidx.databinding.BindingAdapter
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.mostafa.foodapp.R
 import com.mostafa.foodapp.data.model.Categories
 import com.mostafa.foodapp.databinding.ListItemBinding
 
-class CategoryRecyclerView(allData: ArrayList<Categories>) :
-    RecyclerView.Adapter<CategoryRecyclerView.CategoryViewHolder>() {
-    private lateinit var binding: ListItemBinding
+class CategoryRecyclerView(allData: ArrayList<Categories>)
+    : RecyclerView.Adapter<CategoryRecyclerView.CategoryViewHolder>() {
     var categoiesList = mutableListOf<Categories.Category>()
 
+    inner class CategoryViewHolder(val listItemBinding: ListItemBinding) :
+        RecyclerView.ViewHolder(listItemBinding.root) {
 
+    }
     fun setCategoryList(categoiesList: ArrayList<Categories.Category>) {
         this.categoiesList = categoiesList.toMutableList()
         notifyDataSetChanged()
     }
 
-    @SuppressLint("NotifyDataSetChanged")
-    class CategoryViewHolder(val binding: ListItemBinding) :
-        RecyclerView.ViewHolder(binding.root) {
+    override fun getItemCount(): Int = categoiesList.size
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        CategoryViewHolder(
+            DataBindingUtil.inflate<ListItemBinding>(
+                LayoutInflater.from(parent.context),
+                R.layout.list_item,
+                parent,
+                false
+            )
+        )
 
-    }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
-//        binding = ListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-//        return CategoryViewHolder(binding)
-        val inflater = LayoutInflater.from(parent.context)
-        val binding = ListItemBinding.inflate(inflater, parent, false)
-        return CategoryViewHolder(binding)
-    }
 
     override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
-        val categories = categoiesList[position]
-        holder.binding.title.text = categories.strCategory
-        Glide.with(holder.itemView.context).load(categories.strCategoryThumb)
-            .into(holder.binding.strCategoryThumb)
+        holder.listItemBinding.model = categoiesList[position]
     }
 
-    override fun getItemCount(): Int {
-        return categoiesList.size
-    }
+
 }
-//    companion object {
-//        @JvmStatic
-//        @BindingAdapter("loadImage")
-//        fun loadImage(thumbs: ImageView, url: String) {
-//            Glide.with(thumbs)
-//                .load(url)
-//                .circleCrop()
-//                .placeholder(R.drawable.ic_launcher_foreground)
-//                .error(R.drawable.ic_launcher_foreground)
-//                .fallback(R.drawable.ic_launcher_foreground)
-//                .into(thumbs)
-//        }
-//    }
-//}
 
 
 
